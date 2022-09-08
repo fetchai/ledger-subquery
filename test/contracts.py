34ac45afabe82a651ced929c8867e575f53d060e
Joe Sumner <Jonathansumner98@gmail.com>
2022-09-08 14:04:20 +0100

    test: include e2e test for CW20 transfer

--- test/contracts.py
+++ test/contracts.py
@@ -4,7 +4,6 @@ import requests, os
 from cosmpy.aerial.contract import LedgerContract
 from cosmpy.aerial.client import LedgerClient
 from cosmpy.aerial.wallet import Wallet
-from typing import List
 
 
 @dataclass_json
@@ -21,16 +20,68 @@ class BridgeContractConfig:
     next_swap_id: int
 
 DefaultBridgeContractConfig = BridgeContractConfig(
-    cap = "250000000000000000000000000",
-    reverse_aggregated_allowance = "3000000000000000000000000",
-    reverse_aggregated_allowance_approver_cap = "3000000000000000000000000",
-    lower_swap_limit = "1",
-    upper_swap_limit = "1000000000000000000000000",
-    swap_fee = "0",
-    paused_since_block = 18446744073709551615,
-    denom = "atestfet",
-    next_swap_id = 0
+    cap="250000000000000000000000000",
+    reverse_aggregated_allowance="3000000000000000000000000",
+    reverse_aggregated_allowance_approver_cap="3000000000000000000000000",
+    lower_swap_limit="1",
+    upper_swap_limit="1000000000000000000000000",
+    swap_fee="0",
+    paused_since_block=18446744073709551615,
+    denom="atestfet",
+    next_swap_id=0
 )
+
+
+@dataclass_json
+@dataclass
+class CW20BalanceObject:
+    amount: int
+    address: str
+
+
+@dataclass_json
+@dataclass
+class CW20ContractConfig:
+    name: str
+    symbol: str
+    decimals: int
+    initial_balances: list[dict[str, str]]
+
+
+DefaultCW20ContractConfig = CW20ContractConfig(
+    name="test coin",
+    symbol="TEST",
+    decimals=6,
+    initial_balances=[{
+            "amount": "3000000",
+            "address": "fetch1wurz7uwmvchhc8x0yztc7220hxs9jxdjdsrqmn"  # validator address
+    }]
+)
+
+
+class CW20Contract(LedgerContract):
+
+    def __init__(self, client: LedgerClient, admin: Wallet, cfg: CW20ContractConfig):
+        url = "https://github.com/CosmWasm/cw-plus/releases/download/v0.14.0/cw20_base.wasm"
+        if not os.path.exists(".contract"):
+            os.mkdir(".contract")
+        try:
+            temp = open(".contract/cw20.wasm", "rb")
+            temp.close()
+        except:
+            contract_request = requests.get(url)
+            file = open(".contract/cw20.wasm", "wb")
+            file.write(contract_request.content)
+            file.close()
+
+        super().__init__(".contract/cw20.wasm", client)
+
+        self.deploy(
+            cfg.to_dict(),
+            admin,
+            store_gas_limit=3000000
+        )
+
 
 class BridgeContract(LedgerContract):
 
@@ -50,7 +101,7 @@ class BridgeContract(LedgerContract):
         # LedgerContract will attempt to discover any existing contract having the same bytecode hash
         # see https://github.com/fetchai/cosmpy/blob/master/cosmpy/aerial/contract/__init__.py#L74
         super().__init__(".contract/bridge.wasm", client)
-        
+
         # deploy will store the contract only if no existing contracts was found during init.
         # and it will instantiate the contract only if contract.address is None
         # see: https://github.com/fetchai/cosmpy/blob/master/cosmpy/aerial/contract/__init__.py#L168-L179
