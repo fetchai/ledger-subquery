40f1b5de56bd64494b83f2e573ecf0ed44467688
jonathansumner <jonathansumner98@gmail.com>
2023-03-29 13:17:35 +0100

    test: include genesis contract coverage

---
+++ tests/e2e/processing/test_contracts.py
@@ -0,0 +1,70 @@
+import unittest
+from typing import List
+
+from gql import gql
+
+from src.genesis.helpers.field_enums import Contracts
+from src.genesis.processing.contracts import ContractsManager
+from tests.helpers.clients import TestWithDBConn, TestWithGQLClient
+from tests.helpers.genesis_data import test_genesis_data, test_wasm_state_contracts
+
+
+class TestContractsManager(TestWithDBConn, TestWithGQLClient):
+    test_manager: ContractsManager
+    completed = False
+    expected_contracts: List[dict] = [
+        {"id": b["contract_address"]} for b in test_wasm_state_contracts
+    ]
+
+    @classmethod
+    def setUpClass(cls):
+        TestWithDBConn().setUpClass()
+        TestWithGQLClient().setUpClass()
+        cls.truncate_tables("contracts", cascade=True)
+
+        cls.test_manager = ContractsManager(cls.db_conn)
+        cls.test_manager.process_genesis(
+            test_genesis_data
+        )
+
+    def test_sql_retrieval(self):
+        actual_contracts: List[dict] = []
+
+        with self.db_conn.cursor() as db:
+            for row in db.execute(Contracts.select_query()).fetchall():
+                actual_contracts.append(
+                    {
+                        "id": row[Contracts.id.value]
+                    }
+                )
+
+        self.assertListEqual(self.expected_contracts, actual_contracts)
+
+    def test_gql_retrieval(self):
+        actual_contracts: List[dict] = []
+
+        results = self.gql_client.execute(
+            gql(
+                """
+            query {
+                contracts {
+                    nodes {
+                        id
+                    }
+                }
+            }
+        """
+            )
+        )
+
+        for node in results["contracts"]["nodes"]:
+            actual_contracts.append(
+                {"id": node.get("id")}
+            )
+
+        actual_contracts.reverse()
+        self.assertListEqual(self.expected_contracts, actual_contracts)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- tests/helpers/genesis_data.py
+++ tests/helpers/genesis_data.py
@@ -29,6 +29,31 @@ test_bank_state: Dict = {
     "supply": test_bank_state_supply,
 }
 
+test_wasm_state_contracts: List[Dict] = [
+  {
+    "contract_address": "fetch1qxxlalvsdjd07p07y3rc5fu6ll8k4tmetpha8n",
+    "contract_info": {
+      "label": "token-bridge-contract"
+    }
+  },
+  {
+    "contract_address": "fetch1pvrwmjuusn9wh34j7y520g8gumuy9xtljwctjp",
+    "contract_info": {
+      "label": "decibel"
+    }
+  }
+]
+
+test_wasm_state: Dict = {
+    "codes": {},
+    "contracts": test_wasm_state_contracts,
+    "gen_msgs":{},
+    "params":{},
+    "sequences":{},
+}
+
+
+
 test_app_state: Dict = {
     "airdrop": {},
     "auth": {},
@@ -49,7 +74,7 @@ test_app_state: Dict = {
     "transfer": {},
     "upgrade": {},
     "vesting": {},
-    "wasm": {},
+    "wasm": test_wasm_state,
 }
 
 test_genesis_data: Dict = {
