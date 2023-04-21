3e8874e57cbb69e9dd0ac9f0b0e806c2a0ddcad4
jonathansumner <jonathansumner98@gmail.com>
2023-04-21 20:24:47 +0100

    chore: remove node-modules dir for signoz

--- docker/node-cosmos/dist/configure/configure.module.d.ts
+++
@@ -1,5 +0,0 @@
-import { DynamicModule } from '@nestjs/common';
-export declare function validDbSchemaName(name: string): boolean;
-export declare class ConfigureModule {
-    static register(): DynamicModule;
-}
--- docker/node-cosmos/dist/utils/graphql.d.ts
+++
@@ -1,3 +0,0 @@
-import { GraphQLModelsType } from '@subql/utils';
-import { ModelAttributes } from 'sequelize';
-export declare function modelsTypeToModelAttributes(modelType: GraphQLModelsType, enums: Map<string, string>): ModelAttributes;
