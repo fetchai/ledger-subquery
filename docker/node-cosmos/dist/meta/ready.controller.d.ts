011be5e938e869e7b0a4c510c155e34b880301fb
Joey Sumner <47419781+Jonathansumner@users.noreply.github.com>
2023-04-24 17:53:59 +0100

    chore: bump subquery node to v1.19.1 (#259)

--- docker/node-cosmos/dist/meta/ready.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { ReadyService } from './ready.service';
-export declare class ReadyController {
-    private readyService;
-    constructor(readyService: ReadyService);
-    getReady(): void;
-}
