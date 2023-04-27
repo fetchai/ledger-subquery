d7e9ddc8a3708fd22bde4f1ce4c7a27753aa6bc6
jonathansumner <jonathansumner98@gmail.com>
2023-04-27 15:36:02 +0100

    fix: update contract retrival function & endpoint

---
+++ tests/helpers/contract_retrieve.py
@@ -0,0 +1,27 @@
+import os
+import requests
+
+from tqdm import tqdm
+
+
+def ensure_contract(
+    filename: str,
+    bucket_address: str = "https://storage.googleapis.com/fetch-ai-mainnet-artifacts/",
+) -> str:
+    base_path = f".contract"
+    contract_name = os.path.splitext(filename)[0]
+    contract_path = f"{base_path}/{filename}"
+    if not os.path.exists(base_path):
+        os.mkdir(base_path)
+    try:
+        temp = open(contract_path, "rb")
+        temp.close()
+    except OSError:
+        response = requests.get(bucket_address + filename, stream=True)
+        if response.status_code >= 300:
+            raise Exception(f"Contract Download Failed: {contract_name}")
+        with open(contract_path, "wb") as contract:
+            print(f"\ndownloading {contract_name} contract:")
+            for data in tqdm(response.iter_content()):
+                contract.write(data)
+    return contract_path
